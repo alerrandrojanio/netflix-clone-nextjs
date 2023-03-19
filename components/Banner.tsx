aee47b7403cd9b98a98240ecb3100ba3051666da
Alerrandro Janio <alerrandro.janio07@gmail.com>
2023-03-19 20:45:14 -0300

    feat: modal e react-player trailer

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,6 +4,8 @@ import { baseURL } from "../constants/movie"
 import { Movie } from "../typings"
 import { FaPlay } from "react-icons/fa"
 import { InformationCircleIcon } from "@heroicons/react/24/solid"
+import { useRecoilState } from "recoil"
+import { modalState, movieState } from "../atoms/modalAtom"
 
 interface BannerProps {
   netflixOriginals: Movie[]
@@ -11,6 +13,8 @@ interface BannerProps {
 
 function Banner({ netflixOriginals }: BannerProps) {
   const [movie, setMovie] = useState<Movie | null>(null)
+  const [showModal, setShowModal] = useRecoilState(modalState)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
 
   useEffect(() => {
     setMovie(
@@ -41,7 +45,14 @@ function Banner({ netflixOriginals }: BannerProps) {
           <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />
           Play
         </button>
-        <button className="bannerButton bg-[gray]/70">
+
+        <button
+          className="bannerButton bg-[gray]/70"
+          onClick={() => {
+            setCurrentMovie(movie)
+            setShowModal(true)
+          }}
+        >
           More Info
           <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
         </button>
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import { useRecoilState } from "recoil"
+import { modalState, movieState } from "../atoms/modalAtom"
 import { imageURL } from "../constants/movie"
 import { Movie } from "../typings"
 
@@ -8,8 +10,17 @@ interface ThumbnailProps {
 }
 
 export function Thumbnail({ movie }: ThumbnailProps) {
+  const [showModal, setShowModal] = useRecoilState(modalState)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+    <div
+      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+      onClick={() => {
+        setCurrentMovie(movie)
+        setShowModal(true)
+      }}
+    >
       <Image
         src={`${imageURL}${movie.backdrop_path || movie.poster_path}`}
         className="rounded-sm object-cover md:rounded"
