043dce701a627615a88cc36c6297e3aeb94f850a
Alerrandro Janio <alerrandro.janio07@gmail.com>
2023-03-21 13:15:38 -0300

    style: layout modal completo

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,11 +2,13 @@ import MuiModal from "@mui/material/Modal"
 import { useRecoilState } from "recoil"
 import { modalState, movieState } from "../atoms/modalAtom"
 
-import { XMarkIcon } from "@heroicons/react/24/outline"
+import { HandThumbUpIcon, XMarkIcon } from "@heroicons/react/24/outline"
 import { useEffect, useState } from "react"
 import { Element, Genre } from "../typings"
 import { baseURL } from "../constants/movie"
 import ReactPlayer from "react-player/lazy"
+import { FaPlay, FaVolumeMute, FaVolumeUp } from "react-icons/fa"
+import { PlusIcon } from "@heroicons/react/24/solid"
 
 export function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState)
@@ -45,7 +47,11 @@ export function Modal() {
   }
 
   return (
-    <MuiModal open={showModal} onClose={handleClose}>
+    <MuiModal
+      open={showModal}
+      onClose={handleClose}
+      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+    >
       <>
         <button
           onClick={handleClose}
@@ -63,6 +69,64 @@ export function Modal() {
             playing
             muted={muted}
           />
+
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+            <div className="flex space-x-2">
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
+
+              <button className="modalButton">
+                <PlusIcon className="h-7 w-7" />
+              </button>
+
+              <button className="modalButton">
+                <HandThumbUpIcon className="h-7 w-7" />
+              </button>
+            </div>
+
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <FaVolumeMute className="h-6 w-6" />
+              ) : (
+                <FaVolumeUp className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres.map((genre) => genre.name).join(", ")}
+                </div>
+
+                <div>{movie?.original_language}</div>
+
+                <div>
+                  <span className="text-[gray]">Original language: </span>
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     </MuiModal>
